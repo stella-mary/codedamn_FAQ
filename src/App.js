37f1031bdc2947305b5ea9310ac2b529399f255a
stella-mary <mariastellamary@outlook.com>
2022-10-10 18:14:52 +0530

    Added File

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import data from './data'
+import SingleQuestion from './Question';
+import './App.css';
+
+
+function App() {
+  return (
+    <main>
+    <div className="container">
+      <h3>FAQ</h3>
+      <section className='info'>
+        {data.map((question) => {
+          return(
+      <SingleQuestion
+      title={question.title}
+      info={question.info}
+      key={question.id}
+      />
+          )
+          })}
+    </section>
+    </div>
+    </main>
+  )
+}
+
+export default App;
